c7bbf8cac8e1adc180f65914700736818902d90e
Patipol pongsawat <ezboomgg123@gmail.com>
2025-03-19 11:03:22 +0700

    delete help button and profile to finish

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,23 @@
+'use client'
+
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 const Sidebar = ({ isOpen }) => {
+    const router = useRouter()
+    const handleLogout = async () => {
+        try {
+            const res = await fetch('/api/logout', {
+                method: "POST",
+            })
+            if (!res.ok) {
+                console.log("Logout fail")
+            }
+            const data = await res.json()
+            router.push(data.redirectUrl)
+        } catch (err) {
+            console.log(err)
+        }
+      }
   return (
     <div>
         <div className={`w-64 h-[calc(100vh-64px)] flex flex-col bg-gray-800 absolute transition-transform duration-300 z-20 ${
@@ -11,6 +29,12 @@ const Sidebar = ({ isOpen }) => {
                     </div>
                     <div className="w-[60%] h-full flex items-center justify-start p-2 text-[18px]">Home</div>
                 </Link>
+                <div className="cursor-pointer mt-10 w-full h-10 text-white text-2xl flex items-center justify-center" >
+                    <div className="w-[40%] h-full p-2 flex items-center justify-end">
+                    <span className="material-symbols-outlined">logout</span>
+                    </div>
+                    <div className="w-[60%] h-full flex items-center justify-start p-2 text-[18px]" onClick={handleLogout}>Logout</div>
+                </div>
             </ul>
         </div>
     </div>
